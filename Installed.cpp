a37af76eb3ed9f6202ae28c2c3c1158a86a006fc
Bhalchandra <bsahasrabudhe@gmail.com>
2021-09-02 17:45:13 -0500

    C++ programs

---
+++ Installed.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Installed";
+    return 0;
+}
---
+++ ScreatNum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int screatNum = 7 ;
+    int guess ;
+    while(screatNum !=guess){
+        cout << "Enter the guess number:" ;
+        cin >> guess; 
+
+    }
+    cout << "You Win !";
+    return 0;
+}
