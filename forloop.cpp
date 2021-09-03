4dab4a95b5058dc027bb7be9963d0e41afb90743
Bhalchandra <bsahasrabudhe@gmail.com>
2021-09-03 15:39:47 -0500

    for loop with two different ways print numbers

--- forloop.cpp
+++ forloop.cpp
@@ -3,10 +3,19 @@ using namespace std;
 
 int main()
 {
-    for(int i=1 ; i <=10 ; i++) {
+        for(int i=1 ; i <=10 ; i++) {
     cout << i << endl;
+    
 
     }
+    cout << "------------" << endl;
+    int Nums [] = {2,4,11,12,13} ;
+
+    for (int j=0; j<5 ; j++){
+        cout << Nums[j]  << endl ; 
+
+    }
+
     
     return 0;
 }
